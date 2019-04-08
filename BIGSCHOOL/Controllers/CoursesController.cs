409fc1e7935ce5b3cbb63ad1de8c7d858bdd1f96
= <=>
2019-04-08 22:00:43 +0700

    commit to repository

---
+++ BIGSCHOOL/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BIGSCHOOL.Models;
+using BIGSCHOOL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIGSCHOOL.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new SourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
